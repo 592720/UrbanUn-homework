4565bfde0522e0f8394a92b1e7e71a888d1b1eda
592720 <rjb592720@gmail.com>
2024-09-19 16:23:01 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,14 +1,12 @@
 import random
 
 n = random.choice(range(3, 21))
-unflatten_result = []
+result = []
 
 for i in range(1, n):
-    for j in range(1, n):
+    for j in range(i+1, n):
         if n % (i + j) == 0:
-            list_in_a_list = [i, j]
-            if sorted(list_in_a_list) not in unflatten_result:
-                unflatten_result.append(list_in_a_list)
-result = [xflat for flat in unflatten_result for xflat in flat]
+            result.append(i)
+            result.append(j)
 print(f'{n}', ' = ', *result, sep="")
 
